2dde1706e51a378900aacc169984a662d46de1f3
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-04-07 15:06:37 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,26 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int num) {
+		num = Math.abs(num);
+
+		if (num < 10) {
+			return num;
+		}
+
+		return sumOfDigits(num/10) + sumOfDigits(num%10);
+	}
+
+	public static int factorial(int num) {
+		if (num == 0) {
+			return 1;
+		}
+
+		if (num < 0) {
+			return 0;
+		}
+
+		return factorial(num - 1) * num;
+	}
 
 }
